1ddb416f8ed174b759148ca3985e2e12a438b07e
tuong nguyen <sun@tuongs-iMac.local>
2018-03-22 20:53:27 +0700

    update contract policy

--- contracts/BigBomToken.sol
+++ contracts/BigBomToken.sol
@@ -18,6 +18,13 @@ contract BigbomToken is StandardToken, Ownable {
     uint    public  reserveAmount = 635000000 * 1e18; // 635,000,000
     uint    public  bountyAmount = 40000000 * 1e18; // 40,000,000
 
+    address             public bbFounderCoreStaffWallet;
+    address             public bbAdvisorWallet;
+    address             public bbAirdropWallet;
+    address             public bbNetworkGrowthWallet;
+    address             public bbReserveWallet;
+    address             public bbPublicSaleWallet;
+
     uint    public  saleStartTime;
     uint    public  saleEndTime;
 
@@ -25,15 +32,56 @@ contract BigbomToken is StandardToken, Ownable {
     BigbomPrivateSaleList public privateSaleList;
 
     mapping (address => bool) public frozenAccount;
+    mapping (address => uint) public frozenTime;
+    mapping (address => uint) public maxAllowedAmount;
 
     /* This generates a public event on the blockchain that will notify clients */
-    event FrozenFunds(address target, bool frozen);
+    event FrozenFunds(address target, bool frozen, uint seconds);
     /// @notice `freeze? Prevent | Allow` `target` from sending & receiving tokens
     /// @param target Address to be frozen
     /// @param freeze either to freeze it or not
-    function freezeAccount(address target, bool freeze) onlyOwner public {
-        frozenAccount[target] = freeze;
-        FrozenFunds(target, freeze);
+
+    function checkMaxAllowed(address target)  public  returns (uint) {
+        var maxAmount  = balances[target];
+        if(target == bbFounderCoreStaffWallet){
+            maxAmount = 10000000 * 1e18;
+        }
+        if(target == bbAdvisorWallet){
+            maxAmount = 10000000 * 1e18;
+        }
+        if(target == bbAirdropWallet){
+            maxAmount = 40000000 * 1e18;
+        }
+        if(target == bbNetworkGrowthWallet){
+            maxAmount = 20000000 * 1e18;
+        }
+        if(target == bbReserveWallet){
+            maxAmount = 6350000 * 1e18;
+        }
+        return maxAmount;
+    }
+
+
+    function freezeAccount(address target, bool freeze, uint seconds) onlyOwner public {
+        
+        // if unfreeze
+        if(!freeze){
+            // get End time of frozenAccount
+            var frozenEndTime = frozenTime[target];
+            // if now > frozenEndTime
+            require (now >= frozenEndTime);
+            // unfreeze account
+            frozenAccount[target] = freeze;
+            // set time to 0
+            seconds = 0;           
+        }else{
+            frozenAccount[target] = freeze;
+            
+        }
+        // set endTime = now + seconds to freeze
+        frozenTime[target] = now + seconds;
+        FrozenFunds(target, freeze, seconds);
+        
     }
 
     modifier validDestination( address to ) {
@@ -63,6 +111,25 @@ contract BigbomToken is StandardToken, Ownable {
                 privateSaleList = _privateSaleList;
 
     }
+    function getFreezeTimeDefaut(address target) public returns (uint) {
+        var dfTime  = 0;
+        if(target == bbFounderCoreStaffWallet){
+            dfTime = 24 * 30 * 24 * 3600; // 24 thang
+        }
+        if(target == bbAdvisorWallet){
+            dfTime = 12 * 30 * 24 * 3600;
+        }
+        if(target == bbAirdropWallet){
+            dfTime = 2 * 30 * 24 * 3600;
+        }
+        if(target == bbNetworkGrowthWallet){
+            dfTime = 12 * 30 * 24 * 3600;
+        }
+        if(target == bbReserveWallet){
+            dfTime = 12 * 30 * 24 * 3600;
+        }
+        return dfTime;
+    }
     function BigbomToken(uint startTime, uint endTime, address admin
         ) {
         // Mint all tokens. Then disable minting forever.
@@ -84,6 +151,25 @@ contract BigbomToken is StandardToken, Ownable {
         validDestination(_to)
         validFrom(msg.sender)
         returns (bool) {
+            // check from address is Vesting Address
+            if (msg.sender == bbFounderCoreStaffWallet || msg.sender == bbAdvisorWallet|| msg.sender == bbAirdropWallet|| msg.sender == bbNetworkGrowthWallet|| msg.sender == bbReserveWallet){
+                var withdrawAmount =  maxAllowedAmount[msg.sender];
+                var defaultAllowAmount = checkMaxAllowed(msg.sender);
+                var maxAmount = defaultAllowAmount - withdrawAmount;
+                // _value transfer must <= maxAmount
+                require(maxAmount >= _value);
+
+                // if maxAmount = 0, need to block this msg.sender
+                if((withdrawAmount + _value) == defaultAllowAmount){
+                    // freeze account
+                    freezeAccount( msg.sender, true, getFreezeTimeDefaut(msg.sender));
+                    maxAllowedAmount[msg.sender] = 0;
+                }else{
+                    // set max withdrawAmount
+                    maxAllowedAmount[msg.sender] = withdrawAmount + _value;
+                }
+            }
+
         return super.transfer(_to, _value);
     }
 
@@ -99,7 +185,21 @@ contract BigbomToken is StandardToken, Ownable {
         validDestination(_to)
         validFrom(_from)
         returns (bool) {
-        return super.transferFrom(_from, _to, _value);
+            // check from address is Vesting
+            if (_from == bbFounderCoreStaffWallet || _from == bbAdvisorWallet|| _from == bbAirdropWallet|| _from == bbNetworkGrowthWallet|| _from == bbReserveWallet){
+                var withdrawAmount =  maxAllowedAmount[_from];
+                var maxAmount = checkMaxAllowed(_from) - withdrawAmount;
+
+                require(maxAmount >= _value);
+
+                maxAllowedAmount[_from] = withdrawAmount + _value;
+                // if maxAmount = 0, need to block this _from
+                if(maxAllowedAmount[_from]==checkMaxAllowed(_from)){
+                    // freeze account
+                    freezeAccount( _from, true, getFreezeTimeDefaut(_from));
+                }
+            }
+            return super.transferFrom(_from, _to, _value);
     }
 
     event Burn(address indexed _burner, uint _value);
